6acb1a3c3ea0bccd05a61244abfcc7539842a6a8
ishworii <ishworkhanal21@gmail.com>
2025-09-18 23:02:23 -0400

    types of values

--- value.c
+++ value.c
@@ -25,5 +25,24 @@ void free_value_array(ValueArray* array) {
 }
 
 void print_value(const Value value) {
-    printf("%g",value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");break;
+        case VAL_NUMBER:
+            printf("%g",AS_NUMBER(value));break;
+    }
+
+}
+
+bool values_equal(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL : return  AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NUMBER : return AS_NUMBER(a) == AS_NUMBER(b);
+        case VAL_NIL : return true;
+            default: return false; //unreachable
+    }
 }
